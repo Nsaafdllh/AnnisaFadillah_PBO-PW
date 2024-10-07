2da24bb5f61a9420875d7ed2e6ace15c46100efa
Nsaafdllh <annisafadillah125@upi.edu>
2024-10-07 15:14:38 +0700

    Add files via upload
    bapak mohon maaf ini yg udh ditambah polymorphism

--- PBO Pertemuan 4/Tugas.js
+++ PBO Pertemuan 4/Tugas.js
@@ -1,53 +1,104 @@
-class Kapal {
-    constructor(nama, tahunPembuatan) {
-      this.nama = nama;
-      this.tahunPembuatan = tahunPembuatan;
-    }
-  
-    informasiKapal() {
-      return `Kapal ini bernama ${this.nama} dan dibuat pada tahun ${this.tahunPembuatan}`;
-    }
+class PembelianTiket {
+  constructor(nama, tahunPeluncuran, kapasitasPenumpang, hargaTiket) {
+    this.nama = nama;
+    this.tahunPeluncuran = tahunPeluncuran;
+    this.kapasitasPenumpang = kapasitasPenumpang;
+    this.hargaTiket = hargaTiket;
   }
-  
-  class KapalPenumpang extends Kapal {
-    constructor(nama, tahunPembuatan, kapasitasPenumpang) {
-      super(nama, tahunPembuatan);
-      this.kapasitasPenumpang = kapasitasPenumpang;
-    }
-  
-    informasiPenumpang() {
-      return `${super.informasiKapal()} dengan kapasitas penumpang ${this.kapasitasPenumpang}`;
-    }
+
+  beliTiket(jumlahTiket) {
+    const totalHarga = this.hargaTiket * jumlahTiket;
+    return `Pembelian ${jumlahTiket} tiket berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Kapasitas: ${this.kapasitasPenumpang}, Harga per tiket: Rp${this.hargaTiket}`;
   }
-  
-  class PembelianTiket extends KapalPenumpang {
-    constructor(nama, tahunPembuatan, kapasitasPenumpang, hargaTiket) {
-      super(nama, tahunPembuatan, kapasitasPenumpang);
-      this.hargaTiket = hargaTiket;
-    }
-  
-    beliTiket(jumlahTiket) {
-      const totalHarga = this.hargaTiket * jumlahTiket;
-      return `Pembelian ${jumlahTiket} tiket berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Kapasitas: ${this.kapasitasPenumpang}, Harga per tiket: Rp${this.hargaTiket}`;
-    }
+}
+
+class PembelianRuangan extends PembelianTiket {
+  constructor(nama, tahunPeluncuran, kapasitasPenumpang, hargaRuangan) {
+    super(nama, tahunPeluncuran, kapasitasPenumpang);
+    this.hargaRuangan = hargaRuangan;
   }
-  
-  class PembelianRuangan extends KapalPenumpang {
-    constructor(nama, tahunPembuatan, kapasitasPenumpang, hargaRuangan) {
-      super(nama, tahunPembuatan, kapasitasPenumpang);
-      this.hargaRuangan = hargaRuangan;
-    }
-  
-    beliRuangan(jumlahRuangan) {
-      const totalHarga = this.hargaRuangan * jumlahRuangan;
-      return `Pembelian ${jumlahRuangan} ruangan berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Kapasitas: ${this.kapasitasPenumpang}, Harga per ruangan: Rp${this.hargaRuangan}`;
-    }
+
+  beliRuangan(jumlahRuangan) {
+    const totalHarga = this.hargaRuangan * jumlahRuangan;
+    return `Pembelian ${jumlahRuangan} ruangan berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Kapasitas: ${this.kapasitasPenumpang}, Harga per ruangan: Rp${this.hargaRuangan}`;
   }
-  
-  const pembelianTiket1 = new PembelianTiket("Kapal Laut Budiono Siregar", 2010, 500, 200000);
-  
-  const pembelianRuangan1 = new PembelianRuangan("Kapal Laut Budiono Siregar", 2010, 500, 1500000);
-  
-  console.log(pembelianTiket1.beliTiket(3));  // Membeli 3 tiket
-  console.log(pembelianRuangan1.beliRuangan(2));  // Membeli 2 ruangan
-  
+}
+
+// Subclass 1: PembelianVIP
+class PembelianVIP extends PembelianTiket {
+  constructor(nama, tahunPeluncuran, kapasitasPenumpang, hargaVIP) {
+    super(nama, tahunPeluncuran, kapasitasPenumpang, hargaVIP);
+  }
+
+  beliTiket(jumlahTiket) {
+    const totalHarga = this.hargaTiket * jumlahTiket;
+    return `Pembelian ${jumlahTiket} tiket VIP berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Kapasitas: ${this.kapasitasPenumpang}, Harga per tiket VIP: Rp${this.hargaTiket}`;
+  }
+}
+
+// Subclass 2: PembelianEkonomi
+class PembelianEkonomi extends PembelianTiket {
+  constructor(nama, tahunPeluncuran, kapasitasPenumpang, hargaEkonomi) {
+    super(nama, tahunPeluncuran, kapasitasPenumpang, hargaEkonomi);
+  }
+
+  beliTiket(jumlahTiket) {
+    const totalHarga = this.hargaTiket * jumlahTiket;
+    return `Pembelian ${jumlahTiket} tiket ekonomi berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Kapasitas: ${this.kapasitasPenumpang}, Harga per tiket ekonomi: Rp${this.hargaTiket}`;
+  }
+}
+
+// Subclass 3: PembelianKargo
+class PembelianKargo extends PembelianTiket {
+  constructor(nama, tahunPeluncuran, kapasitasKargo, hargaKargo) {
+    super(nama, tahunPeluncuran, kapasitasKargo);
+    this.hargaKargo = hargaKargo;
+  }
+
+  beliKargo(jumlahKargo) {
+    const totalHarga = this.hargaKargo * jumlahKargo;
+    return `Pembelian ${jumlahKargo} kargo berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Kapasitas kargo: ${this.kapasitasPenumpang}, Harga per kargo: Rp${this.hargaKargo}`;
+  }
+}
+
+// Subclass 4: PembelianMakanan
+class PembelianMakanan extends PembelianTiket {
+  constructor(nama, tahunPeluncuran, hargaMakanan) {
+    super(nama, tahunPeluncuran);
+    this.hargaMakanan = hargaMakanan;
+  }
+
+  beliMakanan(jumlahMakanan) {
+    const totalHarga = this.hargaMakanan * jumlahMakanan;
+    return `Pembelian ${jumlahMakanan} makanan berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Harga per makanan: Rp${this.hargaMakanan}`;
+  }
+}
+
+// Subclass 5: PembelianMinuman
+class PembelianMinuman extends PembelianTiket {
+  constructor(nama, tahunPeluncuran, hargaMinuman) {
+    super(nama, tahunPeluncuran);
+    this.hargaMinuman = hargaMinuman;
+  }
+
+  beliMinuman(jumlahMinuman) {
+    const totalHarga = this.hargaMinuman * jumlahMinuman;
+    return `Pembelian ${jumlahMinuman} minuman berhasil. Total harga: Rp${totalHarga}. Kapal: ${this.nama}, Harga per minuman: Rp${this.hargaMinuman}`;
+  }
+}
+
+// Testing
+const pembelianVIP = new PembelianVIP("Kapal Laut Budiono Siregar", 2010, 500, 500000);
+console.log(pembelianVIP.beliTiket(2));  // Membeli 2 tiket VIP
+
+const pembelianEkonomi = new PembelianEkonomi("Kapal Laut Budiono Siregar", 2010, 500, 100000);
+console.log(pembelianEkonomi.beliTiket(4));  // Membeli 4 tiket ekonomi
+
+const pembelianKargo = new PembelianKargo("Kapal Laut Budiono Siregar", 2010, 1000, 2000000);
+console.log(pembelianKargo.beliKargo(3));  // Membeli 3 kargo
+
+const pembelianMakanan = new PembelianMakanan("Kapal Laut Budiono Siregar", 2010, 50000);
+console.log(pembelianMakanan.beliMakanan(5));  // Membeli 5 makanan
+
+const pembelianMinuman = new PembelianMinuman("Kapal Laut Budiono Siregar", 2010, 30000);
+console.log(pembelianMinuman.beliMinuman(3));  // Membeli 3 minuman
